18d33ef713373a73c2499d2a000254ce20df0782
larsb-dev <github@worldagainst.me>
2025-07-27 14:55:29 +0200

    remove single quotes for aliases without spaces and replace those with spaces with backticks

--- queries.sql
+++ queries.sql
@@ -23,21 +23,21 @@ WHERE KundenID = 1;
 
 # °E	Mit welchem SQL-Statment können Sie die Gesamtzahl aller Buchtitel finden?
 
-SELECT COUNT(*) AS 'Erfasste Bücher' FROM Buch;
+SELECT COUNT(*) AS `Erfasste Bücher` FROM Buch;
 
 # °F	Brechnen Sie die Tagesumsätze sowie den mittleren Tagesumsatz. Ordnen Sie nach Datum.
 
-SELECT SUM(Betrag) AS 'Umsatz', AVG(Betrag) AS 'Durchschnittlicher Umsatz'
+SELECT SUM(Betrag) AS Umsatz, AVG(Betrag) AS `Durchschnittlicher Umsatz`
 FROM Bestellung
 GROUP BY BestDatum
 ORDER BY BestDatum;
 
 # °G	Brechnen Sie pro Buchttitel (= "BuchID") die verkaufte Menge, den Umsatz und listen Sie das Ergebnis nach Verkaufsmenge (top-down).
 
-SELECT BuchID, SUM(Menge) AS 'Absatz', SUM(Menge * PreisProStueck) AS 'Umsatz'
+SELECT BuchID, SUM(Menge) AS Absatz, SUM(Menge * PreisProStueck) AS Umsatz
 FROM Bestellposition
 GROUP BY BuchID
-ORDER BY 'Absatz' DESC;
+ORDER BY Absatz DESC;
 
 # °H	Sie interessieren sich für alle Buchtitel, die in einem gewissen Preisspektrum liegen (z.B. zwischen 10.- und 30.-).
 # Wie lautet der SQL Befehl dazu?
@@ -55,14 +55,14 @@ WHERE Preis > 10 AND Preis < 30;
 # so first you make groupings of all the Bestellpositions for every order
 # then you use the SUM() function to sum up each row of those groupings and multiply the Menge * PreisProStueck per row
 
-SELECT Bestellung.BestID, SUM(Menge * PreisProStueck) AS 'Gesamtwert Bestellung'
+SELECT Bestellung.BestID, SUM(Menge * PreisProStueck) AS `Gesamtwert Bestellung`
 FROM Bestellung
 JOIN Bestellposition ON Bestellung.BestID = Bestellposition.BestID
 GROUP BY Bestellung.BestID;
 
 # °J	Geben Sie eine Liste aus, in der die Kunden nach der Anzahl ihrer Bestellungen sortiert sind.
 
-SELECT Kunde.KundenID, Kunde.Vorname, Kunde.Nachname, COUNT(Bestellung.BestID) AS 'AnzahlBestellungen'
+SELECT Kunde.KundenID, Kunde.Vorname, Kunde.Nachname, COUNT(Bestellung.BestID) AS AnzahlBestellungen
 FROM Kunde
 LEFT JOIN Bestellung ON Kunde.KundenID = Bestellung.KundenID
 GROUP BY Kunde.KundenID
@@ -70,7 +70,7 @@ ORDER BY AnzahlBestellungen DESC;
 
 # Eigene Aufgabe (Zusatz): Summe der Mehrwertsteuer pro Bestellung berechnen
 
-SELECT b.BestID, SUM(bp.PreisProStueck * mwst.MWSt_Satz / 100) AS 'Mehrwertsteuer'
+SELECT b.BestID, SUM(bp.PreisProStueck * mwst.MWSt_Satz / 100) AS Mehrwertsteuer
 FROM Bestellung b
 JOIN Bestellposition bp ON b.BestID = bp.BestID
 JOIN MWSt mwst ON bp.MWStID = mwst.MWStID
